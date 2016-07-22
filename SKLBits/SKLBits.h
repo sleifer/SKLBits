b3306f43c7b463d213d4edff9e9d87f34d454465
Simeon Leifer <simeon@droolingcat.com>
2016-07-21 20:22:59 -0700

    Add project with iOS, macOS, tvOS, and watchOS frameworks

---
+++ SKLBits/SKLBits.h
@@ -0,0 +1,19 @@
+//
+//  SKLBits.h
+//  SKLBits
+//
+//  Created by Simeon Leifer on 7/21/16.
+//  Copyright © 2016 droolingcat.com. All rights reserved.
+//
+
+@import Foundation;
+
+//! Project version number for SKLBits.
+FOUNDATION_EXPORT double SKLBitsVersionNumber;
+
+//! Project version string for SKLBits.
+FOUNDATION_EXPORT const unsigned char SKLBitsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SKLBits/PublicHeader.h>
+
+
